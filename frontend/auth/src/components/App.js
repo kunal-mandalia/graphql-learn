d68dd68f20825de2076b7ee0476c086c10b6b31d
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-01-24 01:44:01 +0000

    feat: setup apollo-client

--- frontend/auth/src/components/App.js
+++ frontend/auth/src/components/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Login from './Login'
+import Dashboard from './Dashboard'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const MY_QUERY = gql`
+  query {
+    login(input:{
+      username:"Kunal"
+      password:"Password"
+    })
+  }
+`
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {JSON.stringify(this.props.data)}
         <header className="App-header">
           <h1 className="App-title">Auth: GraphQL</h1>
         </header>
@@ -16,4 +30,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default graphql(MY_QUERY)(App);
---
+++ frontend/auth/src/components/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => (
+  <div className='dashboard'>
+    Dashboard
+  </div>
+)
+
+export default Dashboard
